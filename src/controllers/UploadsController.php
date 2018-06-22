c49b7ab9f42b6ed440fe365ff01b1a6e717f0389
jinxing.liu <821901008@qq.com>
2018-06-22 22:52:41 +0800

    上传文件导出时间格式化

--- src/controllers/UploadsController.php
+++ src/controllers/UploadsController.php
@@ -13,4 +13,18 @@ class UploadsController extends Controller
      * @var string 定义使用的model
      */
     public $modelClass = 'jinxing\admin\models\Uploads';
+
+    /**
+     * 文件导出数据格式化
+     *
+     * @return array|mixed
+     */
+    public function getExportHandleParams()
+    {
+        $array['created_at'] = $array['updated_at'] = function ($value) {
+            return date('Y-m-d H:i:s', $value);
+        };
+
+        return $array;
+    }
 }
