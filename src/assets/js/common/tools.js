b8a89b6722a1cb317fae20fe2d6e972a0a8eb353
jinxing.liu <821901008@qq.com>
2019-06-28 09:50:10 +0800

    docs: 文档更新

--- src/assets/js/common/tools.js
+++ src/assets/js/common/tools.js
@@ -44,17 +44,22 @@ function validateFile(info) {
 
 // 文件上传
 function aceFileInputAjax(file_input, url) {
-    var $form      = file_input.closest('form'),
-        files      = file_input.data('ace_input_files'),
-        deferred   = new $.Deferred;
+    var $form = file_input.closest('form'),
+        files = file_input.data('ace_input_files'),
+        deferred = new $.Deferred;
     // 没有上传文件
-    if (!files || files.length == 0) { deferred.resolve();return deferred.promise();}
+    if (!files || files.length === 0) {
+        deferred.resolve();
+        return deferred.promise();
+    }
 
     // 数据提交的处理
-    if ("FormData" in window ) {
+    if ("FormData" in window) {
         formData_object = new FormData();
         // 表单数据
-        $.each($form.serializeArray(), function(i, item) {formData_object.append(item.name, item.value);});
+        $.each($form.serializeArray(), function (i, item) {
+            formData_object.append(item.name, item.value);
+        });
         // 上传文件信息
         formData_object.append(file_input.attr('name'), files[0]);
         file_input.ace_file_input('loading', true);
@@ -69,27 +74,27 @@ function aceFileInputAjax(file_input, url) {
             data: formData_object
         });
     } else {
-        var temporary_iframe_id = 'temporary-iframe-'+(new Date()).getTime()+'-'+(parseInt(Math.random()*1000));
+        var temporary_iframe_id = 'temporary-iframe-' + (new Date()).getTime() + '-' + (parseInt(Math.random() * 1000));
         var temp_iframe =
-            $('<iframe id="'+temporary_iframe_id+'" name="'+temporary_iframe_id+'" \
+            $('<iframe id="' + temporary_iframe_id + '" name="' + temporary_iframe_id + '" \
 								frameborder="0" width="0" height="0" src="about:blank"\
 								style="position:absolute; z-index:-1; visibility: hidden;"></iframe>')
                 .insertAfter($form);
 
-        $form.append('<input type="hidden" name="temporary-iframe-id" value="'+temporary_iframe_id+'" />');
+        $form.append('<input type="hidden" name="temporary-iframe-id" value="' + temporary_iframe_id + '" />');
         temp_iframe.data('deferrer', deferred);
         $form.attr({
-            method:  'POST',
+            method: 'POST',
             enctype: 'multipart/form-data',
-            target:  temporary_iframe_id
+            target: temporary_iframe_id
         });
 
         file_input.ace_file_input('loading', true);
         $form.get(0).submit();
-        ie_timeout = setTimeout(function(){
+        ie_timeout = setTimeout(function () {
             ie_timeout = null;
             temp_iframe.attr('src', 'about:blank').remove();
-            deferred.reject({'status':'fail', 'message':'Timeout!'});
+            deferred.reject({'status': 'fail', 'message': 'Timeout!'});
         }, 30000);
     }
 
@@ -106,15 +111,15 @@ function aceFileUpload(select, sFileUploadUrl) {
         ie_timeout = null,
         aParams = {
             // 允许上传的文件类型
-            allowExt: allowExt ? allowExt : ['jpg', 'jpeg', 'png', 'gif'],
-            maxSize: maxSize ? maxSize : 200000000,
-            denyExt: denyExt ? denyExt : ['exe', 'php']
+            allowExt: allowExt || ['jpg', 'jpeg', 'png', 'gif'],
+            maxSize: maxSize || 200000000,
+            denyExt: denyExt || ['exe', 'php']
         },
         oOther,
         field = $input.attr("input-name");
 
     if (allowMime) aParams["allowMime"] = allowMime;
-    if ($input.attr('input-type') == 'ace_file') {
+    if ($input.attr('input-type') === 'ace_file') {
         oOther = {
             no_file: '没有选择文件 ...',
             btn_choose: '选择',
@@ -140,7 +145,7 @@ function aceFileUpload(select, sFileUploadUrl) {
     sFileUploadUrl += "sField=" + field;
 
     // 删除操作
-    aParams["before_remove"] = function(){
+    aParams["before_remove"] = function () {
         var v = $file.val();
         if (v) {
             var arr = {};
@@ -157,28 +162,28 @@ function aceFileUpload(select, sFileUploadUrl) {
         return true;
     };
 
-    $input.ace_file_input(aParams).on('change', function() {
+    $input.ace_file_input(aParams).on('change', function () {
         var deferred = aceFileInputAjax($input, sFileUploadUrl);
         // 成功执行
-        deferred.done(function(json) {
-            if (json.code == 0) {
+        deferred.done(function (json) {
+            if (json.code === 0) {
                 layer.msg("上传文件的地址为：" + json.data.sFilePath, {icon: 6});
                 $file.val(json.data.sFilePath);
             } else {
                 layer.msg("上传文件出现错误Error:" + json.msg, {icon: 5});
                 $input.ace_file_input('apply_settings').ace_file_input('reset_input');
             }
-        }).fail(function() {
+        }).fail(function () {
             ajaxFail();
-        }).always(function() {
-            if(ie_timeout) clearTimeout(ie_timeout);
+        }).always(function () {
+            if (ie_timeout) clearTimeout(ie_timeout);
             ie_timeout = null;
             $input.ace_file_input('loading', false);
         });
 
         deferred.promise();
         // 错误处理
-    }).on('file.error.ace', function(event, info) {
+    }).on('file.error.ace', function (event, info) {
         // 判断错误
         layer.msg('文件上传出现错误：' + validateFile(info), {icon: 5});
         event.preventDefault();
