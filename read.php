2cdc8953d62a35fe83d1074f2b3630e9b3d5e5bf
jinxing.liu <821901008@qq.com>
2019-07-04 15:58:17 +0800

    refactor: 修改文件

--- read.php
+++ read.php
@@ -1,49 +1,67 @@
 <?php
+/**
+ * 执行命令：php ./read.php --read=true --delete=true
+ *
+ * @param bool read 是否重新读取
+ * @param bool delete 是否删除多余文件
+ */
 
-$data = [
-    'code' => 10000,
-    'data' => [],
-    'msg'  => 'ok',
-];
+$params = getopt('', ['read:', 'delete:']);
 
-$dir = './docs';
+// 重新读取文件
+if (isset($params['read']) && in_array($params['read'], ['true', '1'])) {
+    $data = [
+        'code' => 10000,
+        'data' => [],
+        'msg'  => 'ok',
+    ];
 
-$array = scandir($dir);
-if ($array) {
-    foreach ($array as $file) {
-        if (in_array($file, ['.', '..'])) {
+    $dir = './docs';
+
+    $array = scandir($dir);
+    if ($array) {
+        foreach ($array as $file) {
+            if (in_array($file, ['.', '..'])) {
+                continue;
+            }
+
+            $str  = substr($file, -3);
+            $path = $dir . '/' . $file;
+            if (is_file($path) && $str == '.md') {
+                $index                = str_replace('.md', '', $file);
+                $data['data'][$index] = file_get_contents($path);
+            }
+        }
+    }
+
+    file_put_contents('./api/content.json', json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+    echo date('Y-m-d H:i:s') . " 读取文件成功，写入 ./api/content.json \n";
+}
+
+if (isset($params['delete']) && in_array($params['delete'], ['true', '1'])) {
+    $json   = file_get_contents('./asset-manifest.json');
+    $values = array_values(json_decode($json, true));
+    $dir    = './';
+    $files  = scandir($dir);
+
+    $number = 0;
+    // 必须要删除的文件
+    foreach ($files as $file) {
+        // 先排除特殊 和 不需要删除的 . 开头的 .md 结尾的
+        if (
+            in_array($file, ['.', '..', 'docs', 'api', 'read.php', '_config.yml', 'asset-manifest.json']) ||
+            in_array('/yii2-admin/' . $file, $values) ||
+            substr($file, 0, 1) === '.' ||
+            substr($file, -3) === '.md'
+        ) {
             continue;
         }
 
-        $str  = substr($file, -3);
-        $path = $dir . '/' . $file;
-        if (is_file($path) && $str == '.md') {
-            $index                = str_replace('.md', '', $file);
-            $data['data'][$index] = file_get_contents($path);
-        }
-    }
-}
-
-file_put_contents('./api/content.json', json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
-
-$json   = file_get_contents('./asset-manifest.json');
-$values = array_values(json_decode($json, true));
-
-$dir   = './';
-$files = scandir($dir);
-
-// 必须要删除的文件
-foreach ($files as $file) {
-    // 先排除特殊 和 不需要删除的 . 开头的 .md 结尾的
-    if (
-        in_array($file, ['.', '..', 'docs', 'api', 'read.php', '_config.yml', 'asset-manifest.json']) ||
-        in_array('/yii2-admin/' . $file, $values) ||
-        substr($file, 0, 1) === '.' ||
-        substr($file, -3) === '.md'
-    ) {
-        continue;
+        unlink('./' . $file);
+        echo "删除文件: {$file} \n";
+        $number++;
     }
 
-    unlink('./' . $file);
-    echo "删除文件: {$file} \n";
+    echo date('Y-m-d H:i:s') . " 删除文件成功，删除文件数: {$number} \n";
 }
+
